74effbd70e0d1bde3ddd64065158174c092f7abd
Steven Theodore <steventheodore98@gmail.com>
2020-12-14 00:10:37 +0700

    First Commit

---
+++ client/src/App.js
@@ -0,0 +1,48 @@
+import { React, useEffect } from 'react'
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
+import { useDispatch } from 'react-redux'
+
+import { getPosts } from './actions/posts'
+import Form from './components/Form/Form'
+import Posts from './components/Posts/Posts'
+import memoru from './images/memoru.png'
+import useStyle from './styles'
+
+const App = () => {
+  const classes = useStyle()
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(getPosts())
+  }, [dispatch])
+
+  return (
+    <Container maxwidth='lg'>
+      <AppBar className={classes.appBar} position='static' color='inherit'>
+        <Typography className={classes.heading} variant='h2' align='center'>
+          Memoru
+        </Typography>
+        <img className={classes.image} src={memoru} alt='memoru' height='60' />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justify='space-between'
+            alignItems='stretch'
+            spacing={3}
+          >
+            <Grid items xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid items xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
+  )
+}
+
+export default App
