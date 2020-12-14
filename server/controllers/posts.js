9b79da949b5748520684c3118ede7bd17a0179c4
Steven Theodore <steventheodore98@gmail.com>
2020-12-14 22:26:30 +0700

    fully functional update

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -33,9 +33,13 @@ export const updatePost = async (req, res) => {
   if (!mongoose.Types.ObjectId.isValid(_id))
     return res.status(404).send('No post with that id')
 
-  const updatePost = await PostMesage.findByIdAndUpdate(_id, post, {
-    new: true,
-  })
+  const updatePost = await PostMesage.findByIdAndUpdate(
+    _id,
+    { ...post, _id },
+    {
+      new: true,
+    }
+  )
 
   res.json(updatePost)
 }
