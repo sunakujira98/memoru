852e8dbf1c6ddf852adc1f36497839aeb51a04d9
Steven Theodore <steventheodore98@gmail.com>
2020-12-15 00:07:32 +0700

    nbsp

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -65,7 +65,7 @@ const Post = ({ post, setCurrentId }) => {
           onClick={() => dispatch(likePost(post._id))}
         >
           <ThumbUpAltIcon fontSize='small' />
-          Like {post.likeCount}
+          Like &nbsp; {post.likeCount} &nbsp;
         </Button>
         <Button
           size='small'
