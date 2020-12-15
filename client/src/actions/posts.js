1ff6b90676f4c14a6cc2e9c4e75256987d37ad5b
Steven Theodore <steventheodore98@gmail.com>
2020-12-15 21:59:01 +0700

    constants

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,10 +1,10 @@
 import * as api from '../api'
-
+import { CREATE, UPDATE, DELETE, FETCH_ALL } from '../constants/actionTypes'
 // Actions creator
 export const getPosts = () => async (dispatch) => {
   try {
     const { data } = await api.fetchPosts()
-    dispatch({ type: 'FETCH_ALL', payload: data })
+    dispatch({ type: FETCH_ALL, payload: data })
   } catch (error) {
     console.log(error)
   }
@@ -14,7 +14,7 @@ export const createPost = (post) => async (dispatch) => {
   try {
     const { data } = await api.createPost(post)
 
-    dispatch({ type: 'CREATE', payload: data })
+    dispatch({ type: CREATE, payload: data })
   } catch (error) {
     console.log(error)
   }
@@ -23,7 +23,7 @@ export const createPost = (post) => async (dispatch) => {
 export const updatePost = (id, post) => async (dispatch) => {
   try {
     const { data } = await api.updatePost(id, post)
-    dispatch({ type: 'UPDATE', payload: data })
+    dispatch({ type: UPDATE, payload: data })
   } catch (error) {
     console.log(error.message)
   }
@@ -32,7 +32,7 @@ export const updatePost = (id, post) => async (dispatch) => {
 export const deletePost = (id) => async (dispatch) => {
   try {
     await api.deletPost(id)
-    dispatch({ type: 'DELETE', payload: id })
+    dispatch({ type: DELETE, payload: id })
   } catch (error) {
     console.log(error)
   }
@@ -41,7 +41,7 @@ export const deletePost = (id) => async (dispatch) => {
 export const likePost = (id) => async (dispatch) => {
   try {
     const { data } = await api.likePost(id)
-    dispatch({ type: 'UPDATE', payload: data })
+    dispatch({ type: UPDATE, payload: data })
   } catch (error) {
     console.log(error)
   }
--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,16 +1,17 @@
+import { FETCH_ALL, CREATE, DELETE, UPDATE } from '../constants/actionTypes'
+
 export default (posts = [], action) => {
   switch (action.type) {
-    case 'FETCH_ALL':
+    case FETCH_ALL:
       return action.payload
-    case 'CREATE':
+    case CREATE:
       return [...posts, action.payload]
-    case 'UPDATE':
-    case 'LIKE':
+    case UPDATE:
       // return an array where only the post that we update is returned
       return posts.map((post) =>
         post._id === action.payload._id ? action.payload : post
       )
-    case 'DELETE':
+    case DELETE:
       // return all the posts except the one that we delete (it comes from the id !)
       return posts.filter((post) => post._id !== action.payload)
     default:
