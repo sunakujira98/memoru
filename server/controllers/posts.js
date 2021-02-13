d1609e05052b5f81709d59b5b8de317931ef1584
Steven Theodore <steventheodore98@gmail.com>
2021-02-14 03:02:35 +0700

    Token Expiry

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -14,7 +14,7 @@ export const getPosts = async (req, res) => {
 export const createPost = async (req, res) => {
   const post = req.body
 
-  const newPost = new postMessage({
+  const newPost = new PostMessage({
     ...post,
     creator: req.userId,
     createdAt: new Date().toISOString(),
