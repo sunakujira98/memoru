34935426a27d424c042479e8b567c751cfb6a253
Steven Theodore <steventheodore98@gmail.com>
2022-04-02 03:10:32 +0700

    Fixing before deployment

--- server/index.js
+++ server/index.js
@@ -3,21 +3,17 @@ import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import path from 'path'
 import postRoutes from './routes/posts.js'
 import userRoutes from './routes/users.js'
 
 const app = express()
 dotenv.config()
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(dirname, '../client/build')))
-
-  app.get('*', (req, res) => res.sendFile(path.resolve(dirname, '../client', 'build', 'index.html')))
-}
+app.use(cors())
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
-app.use(cors())
 
 app.use('/posts', postRoutes)
 app.use('/users', userRoutes)
@@ -35,3 +31,10 @@ mongoose
   .catch((error) => console.log(error.message))
 
 mongoose.set('useFindAndModify', false)
+
+const dirname = path.resolve()
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(dirname, '../client/build')))
+
+  app.get('*', (req, res) => res.sendFile(path.resolve(dirname, '../client', 'build', 'index.html')))
+}
