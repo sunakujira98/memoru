a5410257bb2f7addb1ec2242802cf39d707c0875
Steven Theodore <steventheodore98@gmail.com>
2022-04-02 02:57:13 +0700

    Use dirname instead

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const app = express()
 dotenv.config()
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')))
+  app.use(express.static(path.join(dirname, '../client/build')))
 
   app.get('*', (req, res) => res.sendFile(path.resolve(dirname, '../client', 'build', 'index.html')))
 }
