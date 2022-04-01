e1ad62a68d09ea52980e517ffd2b8424ab83370b
Steven Theodore <steventheodore98@gmail.com>
2022-04-02 03:16:23 +0700

    Delete if statement for production

--- server/index.js
+++ server/index.js
@@ -33,8 +33,6 @@ mongoose
 mongoose.set('useFindAndModify', false)
 
 const dirname = path.resolve()
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(dirname, '../client/build')))
+app.use(express.static(path.join(dirname, '../client/build')))
 
-  app.get('*', (req, res) => res.sendFile(path.resolve(dirname, '../client', 'build', 'index.html')))
-}
+app.get('*', (req, res) => res.sendFile(path.resolve(dirname, '../client', 'build', 'index.html')))
